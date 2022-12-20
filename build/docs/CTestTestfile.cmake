3772d6ebd9582dcab845a875b19f6d1e225af5b0
Emilio Martínez <emiliomartinez98@gmail.com>
2022-12-20 14:46:12 -0300

    Created directories for images and scripts

--- build/docs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/emidan19/gr-tempest/docs
-# Build directory: /home/emidan19/gr-tempest/build/docs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/python/qa_fft_peak_fine_sampling_sync_test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-export VOLK_GENERIC=1
-export GR_DONT_LOAD_PREFS=1
-export srcdir="/home/emidan19/gr-tempest/python"
-export GR_CONF_CONTROLPORT_ON=False
-export PATH="/home/emidan19/gr-tempest/build/python":$PATH
-export LD_LIBRARY_PATH="":$LD_LIBRARY_PATH
-export PYTHONPATH=/home/emidan19/gr-tempest/build/swig:$PYTHONPATH
-/usr/bin/python3 /home/emidan19/gr-tempest/python/qa_fft_peak_fine_sampling_sync.py 
