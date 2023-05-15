3c221d1daa22029a533f6ac0c109cb03ab2895d8
Emilio Martinez <emilio.martinez@fing.edu.uy>
2023-05-15 13:47:43 +0000

    Changes on buttonToFileSink for screenshot display and save

--- gr-tempest/python/tempest/buttonToFileSink.py
+++ gr-tempest/python/tempest/buttonToFileSink.py
@@ -12,24 +12,31 @@ from gnuradio import gr
 import pmt
 import struct
 import cmath
+import matplotlib.pyplot as plt
+from datetime import datetime
+from PIL import Image
 
 class buttonToFileSink(gr.sync_block):
     f"""
     Block that saves num_samples of complex samples after recieving a TRUE boolean message in the 'en' port
     """
-    def __init__(self, Filename = "output.dat", num_samples = 10):
+    def __init__(self, Filename = "output.dat", H_size=800, V_size=600):
         gr.sync_block.__init__(self,
             name="buttonToFileSink",
             in_sig=[(np.complex64)],
             out_sig=[],
         )
         self.Filename = Filename
-        self.num_samples = num_samples
+        self.H_size = H_size
+        self.V_size = V_size
+        self.num_samples = int(H_size*V_size)
         self.en = False #default
         self.remaining2Save = 0
         self.savingSamples = 0
         self.message_port_register_in(pmt.intern("en")) #declare message port
         self.set_msg_handler(pmt.intern("en"), self.handle_msg) #declare handler for messages
+        self.stream_image = [] # initialize list to apppend samples
+
     def work(self, input_items, output_items):      # Don't process, just save samples
         if self.en == True or self.remaining2Save > 0:
             self.available_samples = len(input_items[0]) #available samples at the input for read at this moment
@@ -50,9 +57,45 @@ class buttonToFileSink(gr.sync_block):
         return  self.savingSamples #consume all the samples at the input saved or not 
     
     def save_samples(self, samples):
-        with open(self.Filename, 'ab') as f: #append binary
-            for i in range(0,self.savingSamples):
-               f.write(struct.pack('ff', samples[i].real,samples[i].imag)) #write in float real and imaginary part
+
+        ### Save samples to image stream
+        self.stream_image.extend(samples[:self.savingSamples])
+        
+        ### Show and save image as png if the capture is done
+        if self.remaining2Save == 0:
+
+            date_time = datetime.now().strftime("%d-%m-%Y_%H:%M:%S") # date and time of screenshot
+
+            # Process image values
+            captured_image = np.zeros((self.V_size,self.H_size,3))
+            captured_image_complex = np.array(self.stream_image).reshape((self.V_size,self.H_size))
+            real_part = captured_image_complex.real
+            imag_part = captured_image_complex.imag
+            captured_image[:,:,0] = 255*(real_part - real_part.min())/(real_part.max() - real_part.min())
+            captured_image[:,:,1] = 255*(imag_part - imag_part.min())/(imag_part.max() - imag_part.min())
+
+            # Complex image to uint8
+            captured_image = captured_image.astype('uint8')
+
+            # Empty stream for now upcoming screenshots
+            self.stream_image = [] 
+
+            # Save image as png
+            im = Image.fromarray(captured_image)
+            im.save(self.Filename+'-gr-tempest_screenshot_'+date_time+'.png')
+
+            # Show image
+            plt.figure()
+            plt.title('gr-tempest_screenshot_'+date_time)
+            plt.imshow(captured_image)
+            plt.axis('off')
+            plt.show()
+
+       # ### Save as binary output
+       # with open(self.Filename, 'ab') as f: #append binary
+       #     for i in range(0,self.savingSamples):
+       #        f.write(struct.pack('ff', samples[i].real,samples[i].imag)) #write in float real and imaginary part
+
 
     def handle_msg(self, msg):     #handler of msg
         Msg_value = pmt.cdr(msg) 
